1e0c6bd7e69db732ef7525cab1b272b6ac61ca12
Himanshu <hr1411687@gmail.com>
2025-05-21 11:58:33 +0530

    ok

--- api/index.js
+++ api/index.js
@@ -5,11 +5,7 @@ const cors = require('cors');
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: 'https://your-diary-ruddy.vercel.app/',
-  methods:["POST","GET"],
-  credentials:true // allow frontend domain
-}));
+app.use(cors());
 app.use(express.json()); // to parse JSON bodies
 
 // Connect to MongoDB
